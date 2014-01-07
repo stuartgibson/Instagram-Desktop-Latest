9d470330b5c8bc86938bca07f928d6d6458c647f
George <somu@so.mu>
2014-01-08 02:11:19 +0400

    Refactoring

--- index.php
+++ index.php
@@ -1,30 +1,12 @@
-<?
-	// Get the URL for your own account's self feed at http://instagram.com/developer/endpoints/users/
-	$url='https://api.instagram.com/v1/users/self/feed?access_token={api_key}&count=1';
-	 
-	function get_data( $url )
-	{
-		$ch = curl_init();
-		$timeout = 5;
-		curl_setopt($ch,CURLOPT_URL,$url);
-		curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-		curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
-		$data = curl_exec($ch);
-		curl_close($ch);
-		return $data;
-	}
-	
-	$return = json_decode( get_data($url) );
-	
-	if ( $return->data )
-	{
-		foreach ( $return->data as $p )
-			header("Location: ".$p->images->low_resolution->url);
-	}
-	else
-	{
-		// Either use an image on your server or point to something fun
-		// This will be used if there is an error requesting the data
-		// from Instagram.
-		header("Location: {url_to_suitable_error_image}");
-	}
+<?php
+
+// Either use an image on your server or point to something fun.
+// This will be used if there is an error requesting the data
+// from Instagram.
+$error_image = 'http://distilleryimage3.s3.amazonaws.com/86cf0db877dc11e38302122de019cc27_8.jpg';
+
+// Get your access token at http://blog.pixelunion.net/instagram
+$access_token = '206005842.fdaeaee.402f409181de48b4831fde5f4760ef9c';
+
+$data = json_decode(file_get_contents("https://api.instagram.com/v1/users/self/feed?access_token=$access_token&count=1"))->data;
+header("Location: " . ($data ? $data[0]->images->low_resolution->url : $error_image));
